31ae569f82561adf80abea81b72d593d7e2c7201
Abdullah <abdullah077149@gmail.com>
2022-09-05 12:46:51 +0300

    insert in to

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
 include "connect.php";
-$stmt = $con->prepare("SELECT * FROM users");
-$stmt ->execute();
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $con->prepare("INSERT INTO `users`(`username`, `email`) VALUES ('Hello','Hello@gmail.com')");
+// $stmt->execute();
+// $stmt = $con->prepare("INSERT INTO `users`(`username`, `email`) VALUES (?,?)");
+// $stmt->execute(array("Email","Email@gmail.com"));
+$stmt = $con->prepare("INSERT INTO `users`(`username`, `email`) VALUES (:us,:em)");
+$stmt->execute(array(
+    ":us"=>"Abdullah Tawfeq",
+    ":em"=> "abdullahTawfeeq@gmail.com"
+));
+$count = $stmt->rowCount();
 
-print_r($users);
+if ($count>0) {
+    echo 'success';
+}else{
+    echo 'fail';
+}
+
 ?>
